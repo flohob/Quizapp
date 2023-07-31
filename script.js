4929ca3b949a715f6fad957f5258abf698899aa6
Florian Hobiger <florianhobiger@MacBook-Pro-von-Florian.local>
2023-07-31 17:26:19 +0200

    New Functions in JS

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ currentQuestion = 0;
 
 function init() {
     document.getElementById('allquestions').innerHTML = questions.length;
-
     showquestion();
 }
 
 function showquestion (){
 let question = questions[currentQuestion];
+document.getElementById('numberallquestions').innerHTML = currentQuestion+1;
 document.getElementById('questiontext').innerHTML = question['question'];
 document.getElementById('answer_1').innerHTML = question['answer_1'];
 document.getElementById('answer_2').innerHTML = question['answer_2'];
@@ -21,7 +21,28 @@ function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
 
-    if (selectedQuestionNumber == question['right_answer']) {console.log ("Richige Antwort")}
-     else { console.log("Falsche Antwort")};
+    let idofrightAnswer =`answer_${question['right_answer']}`;
 
+    if (selectedQuestionNumber == question['right_answer']) 
+    {console.log ("Richige Antwort");
+    document.getElementById(selection).parentNode.classList.add('bg-success');
+    document.getElementById('enable-btn').disabled = false;
 }
+     else { console.log("Falsche Antwort")
+     document.getElementById(selection).parentNode.classList.add('bg-danger');
+     document.getElementById(idofrightAnswer).parentNode.classList.add('bg-success');
+     document.getElementById('enable-btn').disabled = false;
+    };}
+
+    function nextQuestion() {
+        currentQuestion++ ;
+        document.getElementById('enable-btn').disabled = true;
+        showquestion();
+        resetAnswerButtons();
+    }
+
+    function resetAnswerButtons() {
+        for (let i = 1; i <= 4; i++) {
+            document.getElementById(`answer_${i}`).parentNode.classList.remove('bg-danger', 'bg-success');
+        }
+    }
