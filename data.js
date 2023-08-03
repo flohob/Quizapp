55445463ad6c828ef5fd76b573fb51b19618b7bd
Florian Hobiger <florianhobiger@MacBook-Pro-von-Florian.local>
2023-08-03 16:52:41 +0200

    Finished Project

--- data.js
+++ data.js
@@ -1,42 +1,42 @@
 let questions = [
     {
-        "question": "Welche der folgenden Teildisziplinen der reinen Informatik gibt es nicht?",
-        "answer_1": "Angewandte Informatik",
-        "answer_2": "Physikalische Informatik",
-        "answer_3": "Technische Informatik",
-        "answer_4": "Theoretische Informatik",
+        "question": "Wie viele Zähne kann ein Hai im Lauf seines Lebens haben?",
+        "answer_1": "Bis zu 10.000 Zähne",
+        "answer_2": "Bis zu 20.000 Zähne",
+        "answer_3": "Bis zu 30.000 Zähne",
+        "answer_4": "Bis zu 40.000 Zähne",
         "right_answer": 2
     },
     {
-        "question": "Was versteht man unter Hypertrophie?",
-        "answer_1": "Muskelaufbau",
-        "answer_2": "Fettverbrennung",
-        "answer_3": "Herz-Kreislauf",
-        "answer_4": "eine Verletzung",
-        "right_answer": 1
+        "question": "Welches der folgenden Nagetiere hält keinen Winterschlaf? ",
+        "answer_1": "Murmeltier",
+        "answer_2": "Siebenschläfer",
+        "answer_3": "Lemming",
+        "answer_4": "Dachs",
+        "right_answer": 3
     },
     {
-        "question": "Welchen Sicherheitsstandard für WLAN sollte man mindestens verwenden?",
-        "answer_1": "WEP",
-        "answer_2": "WPA2",
-        "answer_3": "WPA1",
-        "answer_4": "SSL",
+        "question": "Wer oder Was ist eine Mimose?",
+        "answer_1": "Eine Hauptkrankheit",
+        "answer_2": "Eine tropische Pflanze",
+        "answer_3": "eine Hunderasse",
+        "answer_4": "ein Südseefisch",
         "right_answer": 2
     },
     {
-        "question": "Was ist ein GUI?",
-        "answer_1": "Graphical User Interface",
-        "answer_2": "Generic Unix Interface",
-        "answer_3": "Globally Unique Identity",
-        "answer_4": "Gate Undefined Issue",
-        "right_answer": 1
+        "question": "Welches Tier rennt am schnellsten?",
+        "answer_1": "Eine Schnecke",
+        "answer_2": "Gepard",
+        "answer_3": "Garzelle",
+        "answer_4": "Feldhase",
+        "right_answer": 2
     },
     {
-        "question": "Welches Tag gibt die Hauptüberschrift eines HTML-Dokuments an?",
-        "answer_1": "htop",
-        "answer_2": "head",
-        "answer_3": "mainhead",
-        "answer_4": "h1",
-        "right_answer": 4
+        "question": "Aus welcher Pflanze wird Vanilie gewonnen?",
+        "answer_1": "Rübe",
+        "answer_2": "Orchidee",
+        "answer_3": "Wasserpflanze",
+        "answer_4": "Nussbaum",
+        "right_answer": 2
     }
-];
+];4
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentQuestion = 0;
 let answerSelected = false;
 let rightquestions = 0;
+let questionsAnswered = 0;
 
 function init() {
     document.getElementById('allquestions').innerHTML = questions.length;
@@ -8,31 +9,41 @@ function init() {
 }
 
 function showquestion() {
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endscreen').style = '';
+        document.getElementById('cardbody').style = 'display:none';
+        document.getElementById('endscreen-qu').innerHTML = questions.length;
+        document.getElementById('endscreen-qu1').innerHTML = rightquestions;
 
-if(currentQuestion >= questions.length) {
-    document.getElementById('endscreen').style = '';
-    document.getElementById('cardbody').style = 'display:none';
-    document.getElementById('endscreen-qu').innerHTML = questions.length;
-    document.getElementById('endscreen-qu1').innerHTML = rightquestions;
+        // Check if the quiz is completed
+        if (questionsAnswered === questions.length) {
+            document.getElementById('yay').play();
+        }
 
-} else {
+        document.getElementById('enable-btn').innerHTML = 'Quiz abschließen'; // Change button text here
+    } else {
+        let percentage = Math.round((currentQuestion / questions.length) * 100);
+        document.getElementById('progress-bar').innerHTML = `${percentage}%`;
+        document.getElementById('progress-bar').style.width = `${percentage}%`;
+        console.log(percentage);
+        let question = questions[currentQuestion];
+        document.getElementById('numberallquestions').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
 
-    let percentage = Math.round((currentQuestion / questions.length)*100);
-    document.getElementById('progress-bar').innerHTML = `${percentage}%`;
-    document.getElementById('progress-bar').style.width = `${percentage}%`;
-    console.log(percentage);
-    let question = questions[currentQuestion];
-    document.getElementById('numberallquestions').innerHTML = currentQuestion + 1;
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
-}
+        if (currentQuestion === questions.length - 1) {
+            document.getElementById('enable-btn').innerHTML = 'Quiz abschließen';
+        } else {
+            document.getElementById('enable-btn').innerHTML = 'Nächste Frage';
+        }
+    }
 }
 
 function answer(selection) {
-    if (!answerSelected) { 
+    if (!answerSelected) {
         answerSelected = true;
         let question = questions[currentQuestion];
         let selectedQuestionNumber = selection.slice(-1);
@@ -41,10 +52,17 @@ function answer(selection) {
         if (selectedQuestionNumber == question['right_answer']) {
             rightquestions++;
             document.getElementById(selection).parentNode.classList.add('bg-success');
+            document.getElementById('correctSound').play();
         } else {
             document.getElementById(selection).parentNode.classList.add('bg-danger');
             document.getElementById(idofrightAnswer).parentNode.classList.add('bg-success');
+            document.getElementById('incorrectSound').play();
         }
+
+        questionsAnswered++;
+        let progressPercentage = Math.round((questionsAnswered / questions.length) * 100);
+        document.getElementById('progress-bar').innerHTML = `${progressPercentage}%`;
+        document.getElementById('progress-bar').style.width = `${progressPercentage}%`;
     }
     document.getElementById('enable-btn').disabled = false;
 }
